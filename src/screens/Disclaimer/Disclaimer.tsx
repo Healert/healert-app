5e8274279e87d66011588b530a566b2850fa9ed3
Fatma Ali <alifatma2019@gmail.com>
2020-04-12 18:19:51 +0300

    added disclaimer page

--- src/screens/Disclaimer/Disclaimer.tsx
+++ src/screens/Disclaimer/Disclaimer.tsx
@@ -1,30 +1,28 @@
 import React from 'react'
 import { Text } from 'react-native-elements';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import DisclaimerText from './DisclaimerText';
 
 const Disclaimer = () => {
     const styles = StyleSheet.create({
         container: {
-            display: 'flex',
             alignItems: 'center',
-            justifyContent: 'center'
+            justifyContent: 'center',
+            padding: 15
         },
         title: {
             fontSize: 18,
-            fontWeight: "bold",
-            textDecorationLine: "underline"
+            fontWeight: "500",
+            textAlign: "center"
         }
     });
     return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>Disclaimer</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>
                 {DisclaimerText}
-            <Text>
-                
             </Text>
-        </SafeAreaView>
+        </View>
     )
 }
 
